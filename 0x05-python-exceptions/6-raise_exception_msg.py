a0e2a712f836876fef0e57b9a4f2252017750a2b
Effiong Ekpe <effiongekpe02@gmail.com>
2022-12-19 03:13:00 -0800

    Exceptions

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    '''
+    Raises a name exception with a given message
+    Parameters:
+    message (str): The exception's message
+    Returns:
+    None
+    '''
+    raise NameError(message)
